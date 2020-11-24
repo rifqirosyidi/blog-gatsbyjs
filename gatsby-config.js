4f2f14586201448d8f0eaef77dbcdde53614eab6
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-11-24 13:09:06 +0700

    add some plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,23 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`
       }
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extension: [`.md`, `.mdx`],
+        gatsbyRemarkPlugin: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+              
+            }
+          }
+        ]
+      }
     }
   ],
 }
