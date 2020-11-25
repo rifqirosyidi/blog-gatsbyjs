2e9d64bda288d07c1ba3183a5f3902a420e8d446
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-11-25 11:41:56 +0700

    add nav

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { ContainerWrapper } from "../elements"
-
+import { Nav } from "../components"
 
 export const Container = ({ children }) => {
     return (
-        <ContainerWrapper>{children}</ContainerWrapper>
+        <ContainerWrapper>
+        <Nav />
+            {children}
+        </ContainerWrapper>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,9 @@ import { Container } from "../components"
 
 export default function Home() {
 
-  return <Container>Hello world!</Container>
+  return (
+    <Container>
+      <h1>Hello world!</h1>
+    </Container>
+  )
 }
