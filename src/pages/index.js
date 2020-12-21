2603714456ac7477007a9f391e0deb24e0f88cff
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-12-21 15:03:31 +0700

    display content card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, Card } from "../components"
 
 export default function Home() {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <Card 
+          date="May 22, 2020"
+          title="Lorem ipsum" 
+          excerpt="Commodo exercitation aliqua veniam ut proident labore. Aute sit sunt est ut irure nulla id duis ullamco duis quis ea qui. Veniam ad ullamco laboris nostrud velit proident occaecat est. Duis minim aute labore sint consequat enim veniam tempor aliqua officia deserunt ut ipsum fugiat. Anim in officia cillum consequat elit anim ipsum est eiusmod officia reprehenderit eu occaecat do."
+          slug="/lorem-ipsum" ></Card>
+      </Content>
     </Container>
   )
 }
